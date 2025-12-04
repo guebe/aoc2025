05f088ead9afd48f7e281a172d14e8a7eb8b4176
guebe <57858743+guebe@users.noreply.github.com>
2025-12-04 07:04:17 +0100

    part 1

--- day4/do.py
+++ day4/do.py
@@ -4,19 +4,46 @@ import re
 lines = list()
 
 for line in [line.strip() for line in open(0).readlines()]:
-    x = line.split(' ')
-    a = int(x[2][0:-1])
-    b = int(x[4][0:-1])
-    c = int(x[6][0:-1])
-    d = int(x[8][0:-1])
-    e = int(x[10])
-    lines.append((a,b,c,d,e))
+    x = list(line)
+    lines.append(x)
 
 print(lines)
 
-res = 0
+for l in lines:
+    assert(len(l) == len(lines[0]))
 
-for line in lines:
-    pass
 
-print(res)
+grid = lines
+
+directions=[(-1,-1), (-1,0), (-1,1),
+            (0, -1), (0, 1),
+            (1, -1), (1, 0), (1, 1)]
+
+assert(len(directions) == 8)
+
+rolls = set()
+
+def is_roll(h,w):
+    if w >= 0 and h >= 0 and w < len(grid[0]) and h < len(grid):
+        if grid[h][w] == '@':
+            return True
+    return False
+
+for h in range(len(grid)):
+    for w in range(len(grid[0])):
+        if grid[h][w] == '@':
+            print(f"processing [{h}][{w}]")
+            c = 0
+            for dh,dw in directions:
+                h_ = h + dh
+                w_ = w + dw
+                if (is_roll(h_,w_)):
+                    c += 1
+                    print(f"found {c} roll at [{h_}][{w_}]")
+            if c < 4:
+                print(f"taking [{h}][{w}]")
+                rolls.add((h,w))
+
+print(rolls)
+
+print(len(rolls))
