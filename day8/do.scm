d8d383fdd29e275c59641f007e0c162456e80aca
guebe <57858743+guebe@users.noreply.github.com>
2025-12-05 18:28:16 +0100

    added scheme templates

---
+++ day8/do.scm
@@ -0,0 +1,18 @@
+(import (srfi 1) (srfi 69) (srfi 95) (chicken irregex))
+
+(define example '(
+))
+
+(define input '(
+))
+
+(define (part1 x)
+  x)
+
+(define (part2 x)
+  x)
+
+(display (part1 example)) (newline)
+(display (part2 example)) (newline)
+(display (part1 input)) (newline)
+(display (part2 input)) (newline)
