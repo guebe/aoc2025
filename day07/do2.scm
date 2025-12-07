ab3cbf495b4311d667041fefb3c9d6c7a43251ce
guebe <57858743+guebe@users.noreply.github.com>
2025-12-07 14:20:10 +0100

    day7: merge part1 and part2

--- day07/do2.scm
+++ day07/do2.scm
@@ -167,10 +167,36 @@
     (lambda (pos) (char=? (list-ref splitter pos) #\^))
     (iota (length splitter))))
 
-(define (filter-colide splitters beams)
+(define (part1-filter-colide splitters beams)
+  (filter (lambda (beam) (member beam splitters)) beams))
+
+(define (part1-filter-non-colide splitters beams)
+  (filter (lambda (beam) (not (member beam splitters))) beams))
+
+(define (part1-colide-or-pass splitters beams)
+  (let* ((colisions (part1-filter-colide splitters beams))
+         (non-colisions (part1-filter-non-colide splitters beams))
+	 (left-colisions (map (lambda (x) (- x 1)) colisions))
+	 (right-colisions (map (lambda (x) (+ x 1)) colisions)))
+    (cons
+      (length colisions)
+      (delete-duplicates (append non-colisions left-colisions right-colisions)))))
+
+(define (part1-step state splitters)
+  (let ((num (car state))
+	(beams (cdr state)))
+     (let ((next (part1-colide-or-pass splitters beams)))
+       (let ((new-num (car next))
+	     (new-beams (cdr next)))
+	 (cons (+ num new-num) new-beams)))))
+
+(define (part1 x x-beams)
+  (car (foldl part1-step (cons 0 x-beams) (map filter-splitter (map string->list x)))))
+
+(define (part2-filter-colide splitters beams)
   (filter (lambda (beam) (member (car beam) splitters)) beams))
 
-(define (filter-non-colide splitters beams)
+(define (part2-filter-non-colide splitters beams)
   (filter (lambda (beam) (not (member (car beam) splitters))) beams))
 
 (define (is-same-beam? b1 b2) (let ((b1id (car b1)) (b2id (car b2))) (= b1id b2id)))
@@ -182,9 +208,9 @@
       (cons beam acc)
       (cons (cons (car beam) (+ (cdr (car same-beam)) (cdr beam))) not-same-beam))))
 
-(define (colide-or-pass beams splitters)
-  (let* ((colisions (filter-colide splitters beams))
-         (non-colisions (filter-non-colide splitters beams))
+(define (part2-colide-or-pass beams splitters)
+  (let* ((colisions (part2-filter-colide splitters beams))
+         (non-colisions (part2-filter-non-colide splitters beams))
 	 (left-colisions (map (lambda (x) (let ((beam (car x))(multiplier (cdr x))) (cons (- beam 1) multiplier))) colisions))
 	 (right-colisions (map (lambda (x) (let ((beam (car x))(multiplier (cdr x))) (cons (+ beam 1) multiplier))) colisions)))
     (foldl merge-duplicates '() (append non-colisions left-colisions right-colisions))))
@@ -193,7 +219,9 @@
   (+ state (cdr r)))
 
 (define (part2 x x-beams)
-  (foldl add-beam 0 (foldl colide-or-pass x-beams (map filter-splitter (map string->list x)))))
+  (foldl add-beam 0 (foldl part2-colide-or-pass x-beams (map filter-splitter (map string->list x)))))
 
+(display (part1 example '(7))) (newline)
+(display (part1 input '(70))) (newline)
 (display (part2 example '((7 . 1)))) (newline)
 (display (part2 input '((70 . 1)))) (newline)
