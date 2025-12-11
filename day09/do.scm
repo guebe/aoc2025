70e9631b001681157f2338e8907820010a855e6f
guebe <57858743+guebe@users.noreply.github.com>
2025-12-12 00:20:48 +0100

    cleanup day9

--- day09/do.scm
+++ day09/do.scm
@@ -13,47 +13,47 @@
 
 (include "input.scm")
 
-(define (combinations2 acc li1 li2)
+(define (combinations-impl acc li1 li2)
   (if (null? (cdr li1))
     acc
     (if (null? li2)
-      (combinations2
+      (combinations-impl
 	acc
 	(cdr li1)
 	(cddr li1))
-      (combinations2
+      (combinations-impl
 	(cons (list (car li1) (car li2)) acc)
 	li1
 	(cdr li2)))))
 
 (define (combinations li1)
-  (reverse (combinations2 '() li1 (cdr li1))))
-
-(define (distance2 a b)
-  (let ((p1 (car a))
-	(p2 (cadr a))
-	(q1 (car b))
-	(q2 (cadr b)))
-    (* (+ (abs (- p1 q1)) 1) (+ (abs (- p2 q2)) 1))))
-
-(define (distance li pair)
-  (distance2 (list-ref li (car pair)) (list-ref li (cadr pair))))
-
-(define (distance> a b)
-  (> (car a) (car b)))
-
-(define (part1 x)
-  (caar (take (sort (map (lambda (y) (cons (distance x y) y)) (combinations (iota (length x)))) distance> ) 1)))
-
-(define (get-polygon acc in)
-  (if (null? (cdr in))
-    (reverse acc)
-    (get-polygon (cons (list (car in) (cadr in)) acc) (cdr in))))
+  (reverse (combinations-impl '() li1 (cdr li1))))
 
 (define (get-rectangle li pair)
   (list (list-ref li (car pair)) (list-ref li (cadr pair))))
 
-(define (filter-inside rectangle polygon)
+(define (get-rectangles x)
+  (map (lambda (y) (get-rectangle x y)) (combinations (iota (length x)))))
+
+(define (area rectangle)
+  (let ((x1 (caar rectangle))
+	(y1 (cadar rectangle))
+	(x2 (caadr rectangle))
+	(y2 (cadadr rectangle)))
+    (* (+ (abs (- x1 x2)) 1) (+ (abs (- y1 y2)) 1))))
+
+(define (part1 x)
+  (car (take (sort (map area (get-rectangles x)) >) 1)))
+
+(define (get-polygon-impl acc in)
+  (if (null? (cdr in))
+    (reverse acc)
+    (get-polygon-impl (cons (list (car in) (cadr in)) acc) (cdr in))))
+
+(define (get-polygon x)
+  (get-polygon-impl '() (append x (list (car x)))))
+
+(define (inside? rectangle polygon)
   (let ((x1 (caar rectangle))
 	(y1 (cadar rectangle))
 	(x2 (caadr rectangle))
@@ -69,12 +69,8 @@
 		   (<= (max px1 px2) (min x1 x2))))) polygon)))
 
 (define (part2 x)
-  (let ((polygon (get-polygon '() (append x (list (car x)))))
-	(rectangles (map (lambda (y) (get-rectangle x y)) (combinations (iota (length x))))))
-    (car (take (sort(map (lambda (y) (distance2 (car y) (cadr y)))
-	 (filter (lambda (x) (filter-inside x polygon)) rectangles)
-	 ) >) 1))
-    ))
+  (let* ((polygon (get-polygon x)))
+    (car (take (sort (map area (filter (lambda (y) (inside? y polygon)) (get-rectangles x))) >) 1))))
 
 (display (part1 example)) (newline)
 (display (part1 input)) (newline)
