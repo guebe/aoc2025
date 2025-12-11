5541539a01c5131c5f533296c537dd26053d4c50
guebe <57858743+guebe@users.noreply.github.com>
2025-12-11 18:46:13 +0100

    cleanup day10

--- day10/do2.scm
+++ day10/do2.scm
@@ -1,4 +1,4 @@
-(import (srfi 1) (srfi 95) (chicken bitwise))
+(import (srfi 1) (srfi 95))
 
 (define example '(
 ( #b0110 (3) (1 3) (2) (2 3) (0 2) (0 1) (3 5 4 7))
@@ -7,47 +7,6 @@
 ))
 
 (include "input.scm")
-
-(define (button-combinations lst)
-  (if (null? lst)
-      '(())
-      (let ((rest (button-combinations (cdr lst))))
-        (append rest
-                (map (lambda (x) (cons (car lst) x)) rest)))))
-
-(define (toggle-button acc x)
-  (bitwise-xor acc (arithmetic-shift 1 x)))
-
-(define (toggle-buttons buttons)
-  (foldl toggle-button 0 (apply append buttons)))
-
-(define (find-optimum combinations optimum)
-  (if (null? combinations)
-    10000000000
-    (let* ((buttons (car combinations))
-	   (result (toggle-buttons buttons))
-	   (rest (cdr combinations)))
-      (if (= result optimum)
-        (length buttons)
-        (find-optimum rest optimum)))))
-
-(define (length< a b)
-  (< (length a) (length b)))
-
-(define (part1-line-solver line)
-  (let* ((light-diagram (car line))
-	 (buttons (cdr (take line (- (length line) 1))))
-	 (combinations (sort (button-combinations buttons) length<)))
-    (find-optimum combinations light-diagram)))
-	 
-(define (part1 x)
-  (apply + (map part1-line-solver x)))
-
-(define (add-button acc button)
-  (let ((idx (car acc))
-	(lst (cdr acc)))
-    (display idx) (newline)
-    (display lst) (newline)))
 
 (define (process-buttons result i buttons activation)
   (if (null? buttons)
@@ -86,7 +45,5 @@
 (define (part2 x)
   (map part2-line-solver x))
 
-;(display (part1 example)) (newline)
-;(display (part1 input)) (newline)
 ;(part2 example)
 (part2 input)
