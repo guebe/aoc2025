98d3f9b8eeea1c6e992d8cbe1363f05390109099
guebe <57858743+guebe@users.noreply.github.com>
2026-01-26 00:09:02 +0100

    day1 replaced impl

--- day01/do.scm
+++ day01/do.scm
@@ -1,29 +1,25 @@
 (define example '(-68 -30 48 -5 60 -55 -1 -99 14 -82))
 (load "input.scm")
 
-(define (step1 state r)
-  (let ((x (modulo (+ (car state) r) 100)))
-    (cons x
-	  (+ (cdr state)
-	     (if (= x 0)
-	         1
-	         0)))))
+(define (part1 start result dials)
+  (if (null? dials)
+    result
+    (let ((next (modulo (+ start (car dials)) 100)))
+      (part1 next
+	     (+ result (if (zero? next) 1 0))
+	     (cdr dials)))))
 
-(define (step2 state r)
-  (let ((x (+ (car state) r)))
-    (cons (modulo x 100)
-	  (+ (cdr state)
-	     (if (<= x 0)
-	         (+ (quotient x -100) (if (zero? (car state)) 0 1))
-	         (quotient x 100))))))
+(define (part2 start result dials)
+  (if (null? dials)
+    result
+    (let ((next (+ start (car dials))))
+      (part2 (modulo next 100)
+	     (if (<= next 0)
+	         (+ result (quotient next -100) (if (zero? start) 0 1))
+	         (+ result (quotient next 100)))
+	     (cdr dials)))))
 
-(define (part1 x)
-  (cdr (foldl step1 (cons 50 0) x)))
-
-(define (part2 x)
-  (cdr (foldl step2 (cons 50 0) x)))
-
-(display (part1 example)) (newline)
-(display (part1 input)) (newline)
-(display (part2 example)) (newline)
-(display (part2 input)) (newline)
+(display (part1 50 0 example)) (newline)
+(display (part1 50 0 input)) (newline)
+(display (part2 50 0 example)) (newline)
+(display (part2 50 0 input)) (newline)
