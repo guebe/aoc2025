7e403d4a9b838cc44c5fa7e3e37d354b2e8f628b
guebe <57858743+guebe@users.noreply.github.com>
2026-01-27 23:48:19 +0100

    part1: removed weird accumulator

--- day01/do.scm
+++ day01/do.scm
@@ -1,25 +1,25 @@
 (define example '(-68 -30 48 -5 60 -55 -1 -99 14 -82))
 (load "input.scm")
 
-(define (part1 start result dials)
+; part1: count how often the dial reaches zero exactly
+(define (part1 start dials)
   (if (null? dials)
-    result
+    0
     (let ((next (modulo (+ start (car dials)) 100)))
-      (part1 next
-	     (+ result (if (zero? next) 1 0))
-	     (cdr dials)))))
+      (+ (if (zero? next) 1 0)
+	 (part1 next (cdr dials))))))
 
-(define (part2 start result dials)
+; part2: count how often the dial crosses zero
+(define (part2 start dials)
   (if (null? dials)
-    result
+    0
     (let ((next (+ start (car dials))))
-      (part2 (modulo next 100)
-	     (if (<= next 0)
-	         (+ result (quotient next -100) (if (zero? start) 0 1))
-	         (+ result (quotient next 100)))
-	     (cdr dials)))))
+      (+ (if (<= next 0)
+	     (+ (quotient next -100) (if (zero? start) 0 1))
+	     (quotient next 100))
+	 (part2 (modulo next 100) (cdr dials))))))
 
-(display (part1 50 0 example)) (newline)
-(display (part1 50 0 input)) (newline)
-(display (part2 50 0 example)) (newline)
-(display (part2 50 0 input)) (newline)
+(display (part1 50 example)) (newline)
+(display (part1 50 input)) (newline)
+(display (part2 50 example)) (newline)
+(display (part2 50 input)) (newline)
