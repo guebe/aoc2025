7b017a9fa4329dbf5f91c8d814a8ace39a7774ec
guebe <57858743+guebe@users.noreply.github.com>
2026-02-02 19:24:06 +0100

    improved day03

--- day03/do.scm
+++ day03/do.scm
@@ -1,21 +1,31 @@
-(import (srfi 1))
-
 (define example '(987654321111111 811111111111119 234234234234278 818181911112111))
+(load "input.scm")
 
-(include "input.scm")
+(define (take n lst)
+  (if (or (= n 0) (null? lst))
+      '()
+      (cons (car lst) (take (- n 1) (cdr lst)))))
 
 (define (number->digits number)
-  (map (lambda (x) (- (char->integer x) 48)) (string->list (number->string number))))
+  (define (f number acc)
+    (if (= number 0)
+        acc
+        (f (quotient number 10) (cons (modulo number 10) acc))))
+  (f number '()))
 
 (define (digits->number digits)
-  (string->number (list->string (map (lambda (x) (integer->char (+ x 48))) digits))))
+  (define (f digits acc)
+    (if (null? digits)
+        acc
+	(f (cdr digits) (+ (* acc 10) (car digits)))))
+  (f digits 0))
 
 (define (solve digits k)
   (if (= k 0)
     '()
-    (let* ((window (take digits (- (length digits) (- k 1))))
-	   (best (fold max 0 window))
-	   (rest (cdr (drop-while (lambda (x) (not (= x best))) digits))))
+    (let* ((window (take (- (length digits) k -1) digits))
+	   (best (apply max window))
+	   (rest (cdr (memv best digits))))
       (cons best (solve rest (- k 1))))))
 
 (define (solve-sum in k)
