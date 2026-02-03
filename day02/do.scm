77b1edf929bdb329ff9629364408f495bc8df495
guebe <57858743+guebe@users.noreply.github.com>
2026-02-03 14:12:31 +0100

    10% performance increase

--- day02/do.scm
+++ day02/do.scm
@@ -1,37 +1,37 @@
 (define example '((11 22) (95 115) (998 1012) (1188511880 1188511890) (222220 222224) (1698522 1698528) (446443 446449) (38593856 38593862) (565653 565659) (824824821 824824827) (2121212118 2121212123)))
 (load "input.scm")
 
-(define (repeats-rec? n n-len p p-len start)
-  (if (>= start n-len)
-      #t
-      (let ((end (+ start p-len)))
-        (and (string=? (substring n start end) p)
-             (repeats-rec? n n-len p p-len end)))))
-
 ; returns #t if the string n is made of repeating string p, false otherwise
 (define (repeats? n n-len p)
-  (let ((p-len (string-length p)))
-    (and (> p-len 0)
-	 (< p-len n-len)
-	 (= 0 (modulo n-len p-len))
-         (repeats-rec? n n-len p p-len p-len))))
+  (define p-len (string-length p))
+  (define (recur start)
+    (if (>= start n-len)
+        #t
+        (let ((end (+ start p-len)))
+          (and (string=? (substring n start end) p)
+               (recur end)))))
+  (and (> p-len 0)
+       (< p-len n-len)
+       (= 0 (modulo n-len p-len))
+       (recur p-len)))
 
 ; part 1: check if number is its first half repeated
 (define (part1-valid? n)
-  (let ((n-len (string-length n)))
-    (and (= 0 (modulo n-len 2))
-	 (repeats? n n-len (substring n 0 (quotient n-len 2))))))
-
-(define (part2-valid-rec? n n-len start end)
-  (if (> start end)
-    #f
-    (or (repeats? n n-len (substring n 0 start))
-	(part2-valid-rec? n n-len (+ start 1) end))))
+  (define n-len (string-length n))
+  (define end (quotient n-len 2))
+  (and (= 0 (modulo n-len 2))
+       (repeats? n n-len (substring n 0 end))))
 
 ; part 2: check if number repeats by any pattern
 (define (part2-valid? n)
-  (let ((n-len (string-length n)))
-    (part2-valid-rec? n n-len 1 (quotient n-len 2))))
+  (define n-len (string-length n))
+  (define end (quotient n-len 2))
+  (define (recur start)
+    (if (> start end)
+        #f
+        (or (repeats? n n-len (substring n 0 start))
+	    (recur (+ start 1)))))
+  (recur 1))
 
 (define (inline-display x)
   (display x)
