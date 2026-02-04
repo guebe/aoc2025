16004d149b786cd9ed06e7c484ca40b1f6698c28
guebe <57858743+guebe@users.noreply.github.com>
2026-02-04 03:54:49 +0100

    day4: minor simplify

--- day04/do.scm
+++ day04/do.scm
@@ -1,5 +1,4 @@
 (import (srfi 1))
-
 (define example '(
 "..@@.@@@@."
 "@@@.@.@.@@"
@@ -12,58 +11,73 @@
 ".@@@@@@@@."
 "@.@.@@@.@."
 ))
-
 (load "input.scm")
 (load "../test.scm")
+(load "../srfi-1.scm")
 
-;; point is (row . column)
-;; point is (row * 1024 + column)
+(define *directions* '((-1 . -1) (-1 . 0) (-1 . 1)
+                      (0 . -1)           (0 . 1)
+                      (1 . -1)  (1 . 0)  (1 . 1)))
 
-;; gets all points neighboring (in 8 directions) point, but only if the new
-;; point is inside grid dimensions
-(define (neighbors point rolls)
-  (define directions '((-1 . -1) (-1 . 0) (-1 . 1) (0 . -1) (0 . 1) (1 . -1) (1 . 0) (1 . 1)))
+;; Check paper rolls (@) in on-grid neighbors (8 directions).
+;; Returns #t if >= 4 rolls found (early-exit).
+;; Returns #f otherwise.
+;; This function is tail recursive
+(define (forklift-access? point rolls)
   (define (recur d acc)
-    (if (or (null? d) (>= acc 4))
-        (< acc 4) 
-	(let ((x (car d)))
-          (let ((r (+ (car x) (quotient point 1024)))
-	        (c (+ (cdr x) (modulo point 1024))))
-	    (if (and (>= r 0) (>= c 0) (< r grid-height) (< c grid-width) (memv (+ c (* 1024 r)) rolls))
-	        (recur (cdr d) (+ 1 acc))
-	        (recur (cdr d) acc))))))
-  (recur directions 0))
+    (if (or (null? d) (= acc 4))
+	(< acc 4) 
+	(let* ((dir (car d)) ;; dir is pair (row . column)
+	       (r (+ (car dir) (quotient point 1024)))
+	       (c (+ (cdr dir) (modulo point 1024))))
+	  (if (and (>= r 0) (>= c 0) (< r grid-height) (< c grid-width)
+		   (memv (+ c (* 1024 r)) rolls))
+	      (recur (cdr d) (+ 1 acc))
+	      (recur (cdr d) acc)))))
+  (recur *directions* 0))
 
-(define (get-rolls grid)
-  (append-map (lambda (r) (let ((row (list-ref grid r)))
-			    (filter-map (lambda (c) (if (char=? #\@ (list-ref row c)) (+ c (* r 1024)) #f))
-					(iota grid-width))))
-	      (iota grid-height)))
+;; make a lists of points where we found a roll
+;; point is encoded as number=row*1024 + column
+;; This is O(N) in space and time
+;; This function is tail recursive
+(define (make-rolls grid)
+  (define (scan-rows rows r acc)
+    (if (null? rows)
+        acc
+        (scan-rows (cdr rows) (+ r 1) (scan-cols (car rows) r 0 acc))))
+  (define (scan-cols cols r c acc)
+    (if (null? cols)
+        acc
+        (scan-cols (cdr cols) r (+ c 1)
+                   (if (char=? #\@ (car cols))
+                       (cons (+ c (* r 1024)) acc)
+                       acc))))
+  (scan-rows grid 0 '()))
 
-(define (<4-neighbor-rolls rolls)
-    (filter
-      (lambda (x) (neighbors x rolls))
-      rolls))
+(define (count-forklift-access-1 rolls)
+  (count (lambda (x) (forklift-access? x rolls)) rolls))
 
-(define (part2 rolls)
-  (let loop ((state rolls)
-	     (acc '()))
-    (let ((result (<4-neighbor-rolls state)))
-      (if (null? result)
-	acc
-	(loop
-	  (filter (lambda (point) (not (member point result))) state)
-	  (append acc result))))))
+(define (filter-forklift-access rolls)
+    (filter (lambda (x) (forklift-access? x rolls)) rolls))
+
+(define (count-forklift-access-2 rolls)
+  (define (recur rolls acc)
+    (let ((accessible (filter-forklift-access rolls)))
+      (if (null? accessible)
+	  acc
+	  (recur (filter (lambda (p) (not (memv p accessible))) rolls)
+		 (+ acc (length accessible))))))
+  (recur rolls 0))
 
 (define grid (map string->list example))
 (define grid-height (length grid))
 (define grid-width (length (car grid)))
-(define rolls (get-rolls grid))
-(test (length (<4-neighbor-rolls rolls)) 13)
-(test (length (part2 rolls)) 43)
+(define rolls (make-rolls grid))
+(test (count-forklift-access-1 rolls) 13)
+(test (count-forklift-access-2 rolls) 43)
 (define grid (map string->list input))
 (define grid-height (length grid))
 (define grid-width (length (car grid)))
-(define rolls (get-rolls grid))
-(test (length (<4-neighbor-rolls rolls)) 1367)
-(test (length (part2 rolls)) 9144)
+(define rolls (make-rolls grid))
+(test (count-forklift-access-1 rolls) 1367)
+(test (count-forklift-access-2 rolls) 9144)
